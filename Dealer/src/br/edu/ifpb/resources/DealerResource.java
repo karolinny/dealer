1b28642a004ca22b394c61500c76b92b32c90ee3
karolinny <karolinny1@hotmail.com>
2013-10-28 20:20:08 -0300

    Listando todas as Distribuidoras usando JSON

--- Dealer/src/br/edu/ifpb/resources/DealerResource.java
+++ Dealer/src/br/edu/ifpb/resources/DealerResource.java
@@ -69,6 +69,12 @@ public class DealerResource {
 		
 		dealersMap.put(1, dealer);
 	}
+ 
+ 	@GET
+	@Produces( "application/json")
+	public List<Dealer> getDealer() {
+		return new ArrayList<Dealer>(dealersMap.values());
+	}
 
  	@Path("{id}")
 	@GET
